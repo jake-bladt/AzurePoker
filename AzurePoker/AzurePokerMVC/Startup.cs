74a4d5a1f6e45b2c8e7c6f9d9db5eefa92dc05ea
Jake Bladt <jake@jakebladt.com>
2014-08-29 22:30:28 -0400

    cloud project

---
+++ AzurePoker/AzurePokerMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AzurePokerMVC.Startup))]
+namespace AzurePokerMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
